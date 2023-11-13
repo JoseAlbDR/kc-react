b45a19bb2b0781c52d9931a7a991f31b4f6d3af5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-13 22:50:27 +0100

    feat: counter with toast

--- clases/src/App.tsx
+++ clases/src/App.tsx
@@ -1,11 +1,31 @@
 import './App.css';
 import ReactComponent from './components/ReactComponent';
 import ReactJSX from './components/ReactJSX';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
       <ReactJSX></ReactJSX>;<ReactComponent></ReactComponent>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        gutter={12}
+        containerStyle={{ margin: '8px' }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: '16px 24px',
+          },
+        }}
+      />
     </>
   );
 }
--- clases/src/components/Counter.tsx
+++ clases/src/components/Counter.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
+import toast from 'react-hot-toast';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
   const increaseCount = () => {
     setCount((count) => ++count);
+    toast.success(count.toString());
   };
 
   const decreaseCount = () => {
     setCount((count) => --count);
+    toast.error(count.toString());
   };
 
   return (
